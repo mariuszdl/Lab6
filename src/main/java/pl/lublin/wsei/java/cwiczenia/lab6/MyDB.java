0a5455b2ad5cc415188c43bcefd7408933e0bafc
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-06-16 17:30:31 +0200

    Utworzenie klasy MyDB oraz klasy testującej poprawność jej działania

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab6/MyDB.java
@@ -0,0 +1,73 @@
+package pl.lublin.wsei.java.cwiczenia.lab6;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class MyDB {
+
+    private String host;
+    private Number port;
+    private String dbName;
+    private String user;
+    private String password;
+    private Connection conn = null;
+
+    public MyDB(String host, Number port, String dbName) {
+        this.host = host;
+        this.port = port;
+        this.dbName = dbName;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private void connect() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.put("serverTimezone", "Europe/Warsaw");
+
+        String jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+
+        try {
+            conn = DriverManager.getConnection(jdbcString, connectionProps);
+        } catch (SQLException e) {
+            System.out.println("Błąd podłączenia do bazy: " + jdbcString);
+            System.out.println("Komunikat błędu: " + e.getMessage());
+            conn = null;
+        }
+
+        System.out.println("Connected to database " + dbName);
+    }
+
+    public Connection getConnection() {
+        if (conn == null) {
+            connect();
+        }
+        return conn;
+    }
+
+    public void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Błąd przy zamykaniu połaczenia bazodanowego: " + e.getMessage());
+            }
+        }
+        conn = null;
+    }
+}
